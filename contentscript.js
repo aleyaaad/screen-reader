9f6cc321340c5eec75c0f49bb42b2c9bf8ff71de
aleyaaad <153777158+aleyaaad@users.noreply.github.com>
2026-02-14 05:56:52 -0500

    mostly finished contentscript.js file. added logic for click detection + calls to background.js to capture screen

--- contentscript.js
+++ contentscript.js
@@ -1,7 +1,32 @@
-console.log("Content script loaded!");
+//tells you when/if the content script loads
+let clickCount = 0;
+//resets clickCount if user doesn'y click fast enough
+let clickTimer = null;
 
+/**
+ * document.addEventListener("click", callback) tells the 
+ * page to run this func whenever the user clicks anywhere
+ * event reps the click
+ * 
+ * sets logic to reset clickCount after 
+ * the 1st click if the user doesn't tap fast enough
+ * 
+ */
 document.addEventListener("click", function(event) {
-    if (event.target.tagName === "IMG") {
-        console.log("Image clicked! URL:", event.target.src);
+    clickCount++;
+
+    if (clickCount === 1) {
+        clickTimer = setTimeout(function() {
+            clickCount = 0;
+        }, 400);
+    }
+//clicks reach 2 before 400ms = double click.
+
+    if (clickCount === 2) {
+        clearTimeout(clickTimer);
+        clickCount = 0;
+
+      //sends message to background.js to capture the screen once a double tap is detected
+        chrome.runtime.sendMessage({ type: "CAPTURE_SCREEN" });
     }
 });
